7cc358c6c20ff16c127e012eccc745d445917d81
AlwaysWannaFly <15007159069@163.com>
2020-06-08 23:11:55 +0800

    移动端适配

---
+++ Phone/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    // alert('ready')
+    $('#dowebok').fullpage({
+        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90'],
+        autoScrolling: true,
+        scrollHorizontally: true
+    });
+
+    //methods
+    $.fn.fullpage.setAllowScrolling(false);
+});
