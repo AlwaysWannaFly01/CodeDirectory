6f60815c93603832fb6f252cfe1e3e331aec924d
AlwaysWannaFly <15007159069@163.com>
2020-06-10 00:22:33 +0800

    pc

---
+++ Pc/js/index.js
@@ -0,0 +1,13 @@
+$(function () {
+    $('#dowebok').fullpage({
+        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90', 'salmon', '#000'],
+        loopBottom: true,
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
+        menu: '#menu',
+        'navigation': true,
+    });
+
+    setInterval(function () {
+        $.fn.fullpage.moveSlideRight();
+    }, 3000);
+});
