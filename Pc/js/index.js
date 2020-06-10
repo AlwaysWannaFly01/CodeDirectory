26e8289ae7f247153a647bc85cd0de62d5c84b63
alwayswannafly <15007159069@163.com>
2020-06-10 13:40:44 +0800

    引入bootstrap

--- Pc/js/index.js
+++ Pc/js/index.js
@@ -1,7 +1,7 @@
 $(function () {
     $('#dowebok').fullpage({
         sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90', 'salmon', '#000'],
-        loopBottom: true,
+        loopBottom: false,
         anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
         menu: '#menu',
         'navigation': true,
