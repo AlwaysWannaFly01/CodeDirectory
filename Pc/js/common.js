4b01e2b33aa781b9847d27ec6401173dbdffbe0b
alwayswannafly <15007159069@163.com>
2020-06-12 18:08:53 +0800

    进度条

--- Pc/js/common.js
+++ Pc/js/common.js
@@ -1,11 +1,9 @@
 $(function () {
     $('.menuBtn').click(function () {
-        console.log(123)
         $('.menuList').slideDown(500)
     })
 
     $('.menuList .oper img').click(function () {
         $('.menuList').slideUp(300)
-    })
-
+	})
 })
