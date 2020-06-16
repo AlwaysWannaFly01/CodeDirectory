163ccb8e685c9f9fe43529d42d2b3d6dbc18052e
AlwaysWannaFly <15007159069@163.com>
2020-06-17 01:11:35 +0800

    phone 首页

--- Phone/js/index.js
+++ Phone/js/index.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     // alert('ready')
-    $('#dowebok').fullpage({
-        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90'],
-        autoScrolling: true,
-        scrollHorizontally: true
-    });
+    // $('#dowebok').fullpage({
+    //     sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90'],
+    //     autoScrolling: true,
+    //     scrollHorizontally: true
+    // });
 });
