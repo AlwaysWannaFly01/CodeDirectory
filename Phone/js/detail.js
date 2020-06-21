c991529adb51ee7304a9c183db9a1e9e65213b27
AlwaysWannaFly <15007159069@163.com>
2020-06-21 19:14:24 +0800

    ll

--- Phone/js/detail.js
+++ Phone/js/detail.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-    
+    var navSwiper = new Swiper('.topNav', {
+        freeMode: true,
+        slidesPerView: 'auto',
+        freeModeSticky: true,
+    });
+
+    $('.topNav .swiper-slide').on('click', function (e) {
+        e.preventDefault()
+        $(".topNav .active").removeClass('active')
+        $(this).addClass('active')
+    })
+
 })
