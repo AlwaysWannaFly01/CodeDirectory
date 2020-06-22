d9d32097cd179893b828ffc5305c3e57eb65fc07
alwayswannafly <15007159069@163.com>
2020-06-22 11:22:53 +0800

    pc公共底部

--- Pc/js/index.js
+++ Pc/js/index.js
@@ -12,15 +12,6 @@ $(function () {
     // $('html').addClass('height100')
     $('body').show();
     // $('body').addClass('height100 bg')
-    // $('.aaa').animate({
-    //     'opacity': '1'
-    // }, 500, function () {
-    //     $(this).stop().animate({
-    //         'opacity': '0.9'
-    //     })
-    // })
-    // $('body').addClass('height100 bg fade out brief big')
-
     // NProgress.start();
     // setTimeout(function () {
     NProgress.done();
@@ -31,10 +22,6 @@ $(function () {
     // }
     $('.loading_bg').hide()
     $('.loading_transparent').hide()
-
-    // $('.aaa').animate({
-    //     'opacity': '0'
-    // })
     $('.fade').removeClass('out');
 
     $('#dowebok').fullpage({
@@ -67,11 +54,6 @@ $(function () {
             ) {
                 console.log('滚动到第' + index + '屏')
                 $('.menuAnchor').css('background', '#C82D21')
-                // let liList = $('#menu').find('li')
-                // for (let i = 0; i < liList.length - 1; i++) {
-                //     console.log($(liList[i]).attr('data-menuanchor'));
-                //     $(liList[i]).addClass('albedo')
-                // }
             }
         },
         onLeave: function (index, direction) {
@@ -89,14 +71,6 @@ $(function () {
                 index == '6'
             ) {
                 console.log('离开第' + index + '屏')
-                // if (index === 6) {
-                //     let liList = $('#menu').find('li')
-                //     // console.log(liList);
-                //     for (let i = 0; i < liList.length - 1; i++) {
-                //         console.log($(liList[i]).attr('data-menuanchor'));
-                //         $(liList[i]).removeClass('albedo')
-                //     }
-                // }
                 if (direction == '1') {
                     console.log('进入第1屏')
                     $('.menuAnchor').animate(
@@ -110,20 +84,9 @@ $(function () {
                     )
                 }
             }
-            // if (index == '6') {
-            //     console.log('离开第' + index + '屏')
-            //     let liList = $('#menu').find('li')
-            //     for (let i = 0; i < liList.length - 1; i++) {
-            //         $(liList[i]).removeClass('albedo')
-            //     }
-            // }
 
         },
     })
-
-    // setInterval(function () {
-    //     $.fn.fullpage.moveSlideRight()
-    // }, 3000)
     var bannerSwiper = new Swiper('.banner-swiper-container', {
         autoplay: {
             delay: 3000,
@@ -142,10 +105,10 @@ $(function () {
         slidesPerView: 'auto',
         loop: true,
         /* 自动播放 */
-        autoplay: {
-            delay: 2000,
-            disableOnInteraction: false /* 如果设置为false，用户操作swiper之后自动切换不会停止，每次都会重新启动autoplay */,
-        },
+        // autoplay: {
+        //     delay: 2000,
+        //     disableOnInteraction: false /* 如果设置为false，用户操作swiper之后自动切换不会停止，每次都会重新启动autoplay */,
+        // },
         fadeEffect: true, //true。开启淡出。过渡时，原slide透明度从1->0（淡出），过渡中的slide透明度从0->1（淡入），其他slide透明度0。当你的slide中图片大小不同时可以用到。
         coverflowEffect: {
             rotate: 20, //slide做3d旋转时Y轴的旋转角度
