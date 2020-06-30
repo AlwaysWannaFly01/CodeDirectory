07ac84a8047d14d5a05d2f0bda59221a28295ba1
雷远鹏 <peter2351@qq.com>
2020-06-30 13:22:56 +0800

    fixed

--- Pc/js/about.js
+++ Pc/js/about.js
@@ -10,5 +10,19 @@ $(document).ready(function () {
 		centeredSlidesBounds: false,
 		spaceBetween: 22,
         lazy: true
-    })
-})
+    });
+
+    //默认不开启
+    storeSwiper.autoplay.stop();
+
+    $(document).scroll(function() {
+        sTop = $(this).scrollTop(); //获取滚动条的位置
+        var sh = $(window).height();//视口高度
+        var ft = $(".store-swiper-container").offset().top;
+
+        //滚动条位置
+        if (sTop > (ft - sh)) {
+            storeSwiper.autoplay.start();
+        }
+    });
+});
