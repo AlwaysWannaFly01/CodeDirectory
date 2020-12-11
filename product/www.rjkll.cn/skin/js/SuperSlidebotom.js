4f15df50f9d20f7865ec2f2d69516d00703df559
alwayswannafly <15007159069@163.com>
2020-12-11 16:00:56 +0800

    初始化pc

--- product/www.rjkll.cn/skin/js/SuperSlidebotom.js
+++
@@ -1,8 +0,0 @@
-jQuery("#nav").slide({ type:"menu",titCell:".mgl", targetCell:".mglbox",effect:"slideDown",delayTime:300 ,triggerTime:0,returnDefault:true});
-jQuery(".banner").slide({ titCell:".hd ul", mainCell:".bd ul", effect:"fold",  autoPlay:true, autoPage:true, trigger:"click" });	
-jQuery(".mgcpbox").slide({effect:"fold",autoPlay:true});
-jQuery(".indexnew").slide({effect:"top",autoPlay:true});
- jQuery(".ztgun").slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"top",/*autoPlay:true,*/vis:2,trigger:"click"});
-jQuery(".stroes").slide({ effect:"left", titCell:".hd li",trigger:"click",mainCell:".bd"});
-jQuery(".inBox").slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"left",autoPlay:true,vis:2,trigger:"click"});
-
