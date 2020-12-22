db7218cc640759a9e75e374bd05cddf516d6edc6
alwayswannafly <15007159069@163.com>
2020-12-22 17:54:14 +0800

    store

--- wugu/pc/js/index.js
+++ wugu/pc/js/index.js
@@ -32,4 +32,41 @@ $(function () {
         vis: 2,
         trigger: "click"
     });
+
+    $('#Form #fSubmit').unbind('click').bind('click', function () {
+        var name = $('#fName').val();
+        var phone = $('#fTel').val();
+        var city = $('#fCity').val();
+
+        if (name && phone && city) {
+            if (!(/^1[3456789]\d{9}$/.test(phone))) {
+                alert('手机号格式不正确!');
+                return false;
+            }
+            console.log(name);
+            console.log(phone);
+            console.log(city);
+            $.ajax({
+                url: 'http://101.37.245.205:9111/advisorymessage/addMessage',
+                type: 'get',
+                data: {
+                    'name': name,
+                    'phone': phone,
+                    'city': city,
+                },
+                dataType: 'JSON',
+                success: function (data) {
+                    console.log(data)
+                    alert('提交成功!');
+                }
+            })
+            alert('提交成功!');
+            setTimeout(()=>{
+                window.reload()
+            },1000)
+        } else {
+            alert('内容不能为空!');
+            return false;
+        }
+    })
 })
