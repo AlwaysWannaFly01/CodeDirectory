1b1005e3f895cc721c183eddb60c703913f399af
alwayswannafly <15007159069@163.com>
2021-01-15 16:00:47 +0800

    文章

--- wugu/pc/js/index.js
+++ wugu/pc/js/index.js
@@ -69,4 +69,54 @@ $(function () {
             return false;
         }
     })
+
+    //提交留言
+    $('#message #Submit').unbind('click').bind('click', function () {
+        var name = $('#m-name').val();
+        var phone = $('#m-tel').val();
+        var comment = $('#m-message').val();
+
+        if (name && phone && comment) {
+            if (!(/^1[3456789]\d{9}$/.test(phone))) {
+                alert('手机号格式不正确!');
+                return false;
+            }
+            console.log(name);
+            console.log(phone);
+            console.log(comment);
+            $.ajax({
+                url: 'http://101.37.245.205:9111/advisorymessage/addMessage',
+                type: 'get',
+                data: {
+                    'name': name,
+                    'phone': phone,
+                    'city': comment,
+                },
+                dataType: 'JSON',
+                success: function (data) {
+                    console.log(data)
+                    alert('提交成功!');
+                }
+            })
+            alert('提交成功!');
+            setTimeout(()=>{
+                window.reload()
+            },1000)
+        } else {
+            alert('内容不能为空!');
+            return false;
+        }
+    })
+
+    $('.hexagon_child_child').on('mouseenter', function () {
+        $(this).css({ 'backgroundColor': '#C82D21' })
+        $(this).find('.origin').hide()
+        $(this).find('.change').show().css({"display":'flex'})
+
+    })
+    $('.hexagon_child_child').on('mouseleave', function () {
+        $(this).css({ 'backgroundColor': '#F7B6B2' })
+        $(this).find('.origin').show()
+        $(this).find('.change').hide()
+    })
 })
